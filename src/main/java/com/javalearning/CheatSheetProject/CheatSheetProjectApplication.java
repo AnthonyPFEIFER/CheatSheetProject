3c22daf1af8507c949d109856cbb07c40063f0e2
AnthonyPFEIFER <anthhony.pfeifer@gmail.com>
2021-11-08 17:53:50 +0100

    First commit

---
+++ src/main/java/com/javalearning/CheatSheetProject/CheatSheetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.javalearning.CheatSheetProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheatSheetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheatSheetProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javalearning/CheatSheetProject/CheatSheetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalearning.CheatSheetProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheatSheetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
